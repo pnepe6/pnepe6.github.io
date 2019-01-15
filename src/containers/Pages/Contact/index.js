cf941c77c8aa39c0ffdc6bd125cc2aa6f1ba7c7d
Adrien Gadaud <adrien.gadaud@kopaxgroup.com>
2019-01-15 22:39:50 +0700

    Update Contact page for responsive design.

--- src/containers/Pages/Contact/index.js
+++ src/containers/Pages/Contact/index.js
@@ -7,29 +7,30 @@ class Contact extends Component {
 		return (
 			<div className="contact-ct">
 				<SectionTop>
-					<h1>Contactez-moi</h1>
+					<h1>Contactez moi</h1>
 				</SectionTop>
+				<div className="ct-intro">
+					<p><strong>Je suis disponible pour un CDI et en recherche active d'une équipe dynamique et pourquoi pas bilingue!</strong></p>
+					<p><small>Je suis également ouvert pour collaborer sur de nombreux types de projets qui me passionnent:</small></p>
+					<ul>
+						<li>Projets évenementiels lié au digital, à l'entreprenariat ou encore à la musique et au sport.</li>
+						<li>Projets digitaux Open Source, creation de blog, media internet ou tout projet lié au monde passionnant des jeux vidéo.</li>
+					</ul>
+				</div>
 				<div className="ct-form">
-					<p><strong>Je suis disponible pour un CDI, je suis en recherche active d'une équipe dynamique et pourquoi pas bilingue!</strong></p>
-					<small>Je suis également ouvert pour collaborer sur de nombreux types de projets qui me passionnent:</small>
-					<div>
-						<small>Projets évenementiels lié au digital, à l'entreprenariat ou encore à la musique et au sport.</small>
-					</div>
-					<small>Projets digitaux Open Source, creation de blog, media internet ou tout projet lié au monde passionnant des jeux vidéo.</small>
 					<form action="https://formspree.io/adrien.gadaud@gmail.com" method="POST">
-					    
-					    <label for="email">Email</label>
-					    <input type="email" id="email" name="email" placeholder="Votre Email..." />
-						
-						<select size="0.1" name="object">
-						    <option value="employment">Offre d'emploi</option>
-						    <option value="project">Projet</option>
-						    <option value="other">Autre</option>
-						</select>
-
-					    <label className="input-message" for="message">Message</label>
-					    <input type="text" id="message" name="message" placeholder="Votre message..." />
-
+					    <label for="email">Votre email</label>
+					    <input type="email" id="email" name="email" placeholder="un email valide..." />
+						<div className="form-select">
+							<p>pour</p>
+							<select size="0.1" name="object">
+							    <option value="employment">une offre d'emploi</option>
+							    <option value="project">un projet</option>
+							    <option value="other">autre chose</option>
+							</select>
+						</div>
+					    <label className="input-message" for="message">Votre message</label>
+						<textarea name="message" type="text" placeholder="n'hesitez pas à développer votre demande..."></textarea>
 					    <input type="submit" value="Submit" />
 					</form>
 				</div>
