a1829211e556a1dd5eae91522542f9324c063e73
Adrien Gadaud <adrien.gadaud@kopaxgroup.com>
2019-01-24 23:07:13 +0700

    Fix HMR.

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Helmet } from "react-helmet";
 import ReactGA from 'react-ga';
 import Navigation from '../Navigation';
 import Footer from '../../components/Footer';
@@ -13,11 +12,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Helmet>
-            <meta charSet="utf-8" />
-            <title>Cv chef de projet digital</title>
-        	  <meta name="description" content="Curriculum vitae d'Adrien Gadaud, Chef de projet Web et Chef de projet Digital en région parisienne." />
-        </Helmet>
       	<Navigation />
 		    <Footer />
       </div>
