38444b45698db56f40a8f185c76411a4c6fc0bc1
Adrien Gadaud <adrien.gadaud@kopaxgroup.com>
2019-01-24 21:05:37 +0700

    Add react-hot-loader to fix HMR in chrome.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader'
 import { render } from 'react-snapshot';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-render(<App />, document.getElementById('root'));
+render(<AppContainer><App /></AppContainer>, document.getElementById('root'));
 registerServiceWorker();
 
 if (module.hot) {
   module.hot.accept('./containers/App', () => {
     const NextApp = require('./containers/App').default;
-    render(NextApp);
-	registerServiceWorker();
+    render(<AppContainer><NextApp /></AppContainer>, document.getElementById('root'));
   });
 }
